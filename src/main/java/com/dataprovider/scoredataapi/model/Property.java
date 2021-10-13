4ffbcdf56bda091c6912b8bb4bb84f91ca6ada12
Robertson Douglas Castro Souza <robertson.souza_ntconsult@prestador.globo>
2021-10-13 09:08:49 -0300

    Adding getters and setters

--- src/main/java/com/dataprovider/scoredataapi/model/Property.java
+++ src/main/java/com/dataprovider/scoredataapi/model/Property.java
@@ -5,7 +5,17 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Property {
 
